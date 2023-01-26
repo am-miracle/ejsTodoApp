2f56b0012bbf819513aeab75604900d7eb57cd16
Miracle Jude <judemiraco0@gmail.com>
2023-01-26 16:46:10 +0100

    passing data to the webpage

--- app.js
+++ app.js
@@ -3,67 +3,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true}))
 
 const port = 3000;
+let items = ["Wake Up", "Pray", "Learn Blockchain"]
 
 app.get("/", (req, res) => {
     const today = new Date();
     const currentDay = today.getDay()
-    let day = ''
-
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday'
-            break
-        case 1:
-            day = 'Monday'
-            break
-        case 2:
-            day = 'Tuesday'
-            break
-        case 3:
-            day = 'Wednesday'
-            break
-        case 4:
-            day = 'Thursday'
-            break
-        case 5:
-            day = 'Friday'
-            break
-        case 6:
-            day = 'Saturday'
-            break
-        default:
-            console.log(`Error: current day is equal to: ${currentDay}`);
-            //  throw Error;
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'short'
     }
+    let day = today.toLocaleDateString("en-US", options);
 
-    // if(currentDay === 0){
-    //     day = 'Sunday'
-    // }
-    // else if(currentDay === 1){
-    //     day = 'Monday'
-    // }
-    // else  if(currentDay === 2){
-    //     day = 'Tuesday'
-    // }
-    // else  if(currentDay === 3){
-    //     day = 'Wednesday'
-    // }
-    // else  if(currentDay === 4){
-    //     day = 'Thursday'
-    // }
-    // else  if(currentDay === 5){
-    //     day = 'Friday'
-    // }
-    // else  if(currentDay === 6){
-    //     day = 'Saturday'
-    // }
-    res.render('list', {KindOfDay: day})
+    res.render('list', {
+        KindOfDay: day,
+        newListItems: items
+    })
 
 })
 
-
+app.post("/", (req, res) => {
+    let item = req.body.newItem
+    items.push(item)
+    res.redirect('/')
+})
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`)
